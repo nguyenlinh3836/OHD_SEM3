0ebbddda0c1c7418d27b5e0b66a06d6d0893cea7
nguyenlinh3836 <54592412+nguyenlinh3836@users.noreply.github.com>
2021-02-26 16:58:48 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OHD_SEM3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Facility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OHDProject.Models
+{
+    public class Facility
+    {
+        public int FacilityId { get; set; }
+        public string FacilityName { get; set; }
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
